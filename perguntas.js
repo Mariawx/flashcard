3921ea0357d69a34a5281e5af2d88969e8b79dbd
Mariawx <grzeszcyszen.maria@escola.pr.gov.br>
2025-08-11 11:04:44 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -11,9 +11,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Saúde',
+    'Quanto é retirado para uma doação de sangue?',
+    'São retirados 450 mililitros'
 )
     criaCartao(
     'Lingua inglesa',
